3b4b7df00faab5a5e9a0a6066af7df27549238a5
stoo johnston <stoo@pulumi.com>
2024-04-16 10:32:52 +0100

    Pulumi & AWS: Deploy changes (python)

--- onboarding/aws_get-started_python/quickstart/__main__.py
+++ onboarding/aws_get-started_python/quickstart/__main__.py
@@ -4,13 +4,33 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
-
-# Create an S3 Bucket object
-bucketObject = s3.BucketObject(
-    'index.html',
-    bucket=bucket.id,
-    source=pulumi.FileAsset('./index.html')
+bucket = s3.Bucket(
+    "my-bucket",
+    website=s3.BucketWebsiteArgs(
+        index_document="index.html",
+    ),
 )
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+
+ownership_controls = s3.BucketOwnershipControls(
+    "ownership-controls",
+    bucket=bucket.id,
+    rule=s3.BucketOwnershipControlsRuleArgs(
+        object_ownership="ObjectWriter",
+    ),
+)
+
+public_access_block = s3.BucketPublicAccessBlock(
+    "public-access-block", bucket=bucket.id, block_public_acls=False
+)
+
+bucket_object = s3.BucketObject(
+    "index.html",
+    bucket=bucket.id,
+    source=pulumi.FileAsset("index.html"),
+    content_type="text/html",
+    acl="public-read",
+    opts=pulumi.ResourceOptions(depends_on=[public_access_block, ownership_controls]),
+)
+
+# Export the url of the bucket endpoint
+pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
