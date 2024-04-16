bc2d2aeca5cffbb691e464e78975754c40b85c2f
stoo johnston <stoo@pulumi.com>
2024-04-16 10:21:32 +0100

    Pulumi & AWS: Modify program (python)

--- onboarding/aws_get-started_python/quickstart/__main__.py
+++ onboarding/aws_get-started_python/quickstart/__main__.py
@@ -6,5 +6,11 @@ from pulumi_aws import s3
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('my-bucket')
 
+# Create an S3 Bucket object
+bucketObject = s3.BucketObject(
+    'index.html',
+    bucket=bucket.id,
+    source=pulumi.FileAsset('./index.html')
+)
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
