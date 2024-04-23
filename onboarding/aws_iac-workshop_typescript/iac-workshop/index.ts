b2e2740e354bc093fcd29c7fc59e980e66703313
stoo johnston <stoo@pulumi.com>
2024-04-23 15:13:53 +0100

    1.4 Updating infrastructure

--- onboarding/aws_iac-workshop_typescript/iac-workshop/index.ts
+++ onboarding/aws_iac-workshop_typescript/iac-workshop/index.ts
@@ -1,8 +1,43 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import { Resource } from "@pulumi/aws/apigateway";
 
 const bucket = new aws.s3.Bucket("my-website-bucket", {
     website: {
         indexDocument: "index.html",
     },
 });
+
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock(
+    "public-access-block", {
+        bucket: bucket.bucket,
+        blockPublicPolicy: false,
+    }
+);
+
+new aws.s3.BucketPolicy(
+    "bucket-policy", {
+        bucket: bucket.bucket,
+        policy: pulumi.jsonStringify({
+            Version: "2012-10-17",
+            Statement: [{
+                Effect: "Allow",
+                Principal: "*",
+                Action: ["s3:GetObject"],
+                Resource: [
+                    pulumi.interpolate`${bucket.arn}/*`,
+                ],
+            }]
+        })
+    }, {
+        dependsOn: [publicAccessBlock],
+});
+
+new aws.s3.BucketObject(
+    "index.html", {
+        bucket: bucket,
+        source: new pulumi.asset.FileAsset("www/index.html"),
+        contentType: "text/html",
+});
+
+export const url = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
