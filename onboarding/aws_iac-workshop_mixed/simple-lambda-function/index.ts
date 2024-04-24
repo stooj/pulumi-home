abb2d49d1cf0e773c1c73619050e99c07a8bd7b4
stoo johnston <stoo@pulumi.com>
2024-04-24 15:53:04 +0100

    4.3 Provisioning a lambda function

--- onboarding/aws_iac-workshop_mixed/simple-lambda-function/index.ts
+++ onboarding/aws_iac-workshop_mixed/simple-lambda-function/index.ts
@@ -1,2 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+
+const role = new aws.iam.Role('my-function-role', {
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+        Service: "lambda.amazonaws.com"
+    })
+})
+
+const lambdaFunction = new aws.lambda.Function('my-function', {
+    role: role.arn,
+    handler: "index.handler",
+    // runtime: aws.lambda.NodeJS12dXRuntime,
+    runtime: aws.lambda.Runtime.NodeJS18dX,
+    code: new pulumi.asset.AssetArchive({
+        "index.js": new pulumi.asset.StringAsset(
+            "exports.handler = (e, c, cb) => cb(null, {statusCode: 200, body: 'Hello, world!'});",
+        ),
+    }),
+})
+
+export const functionName = lambdaFunction.name
