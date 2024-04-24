db7def72044f6432490db520fd259c46f4e879d1
stoo johnston <stoo@pulumi.com>
2024-04-24 14:08:21 +0100

    2.4 Add a loadbalancer

--- onboarding/aws_iac-workshop_mixed/iac-workshop-webservers/__main__.py
+++ onboarding/aws_iac-workshop_mixed/iac-workshop-webservers/__main__.py
@@ -3,6 +3,7 @@
 import pulumi
 import pulumi_aws as aws
 
+
 ami = aws.ec2.get_ami(
     most_recent="true",
     owners=["amazon"],
@@ -10,6 +11,14 @@ ami = aws.ec2.get_ami(
 )
 
 vpc = aws.ec2.get_vpc(default=True)
+vpc_subnets = aws.ec2.get_subnets(
+    filters=[
+        aws.ec2.GetSubnetsFilterArgs(
+            name="vpc-id",
+            values=[vpc.id],
+        )
+    ]
+)
 
 group = aws.ec2.SecurityGroup(
     "web-secgrp",
@@ -29,11 +38,38 @@ group = aws.ec2.SecurityGroup(
             "cidr_blocks": ["0.0.0.0/0"],
         },
     ],
+    egress=[
+        {
+            "protocol": "tcp",
+            "from_port": 80,
+            "to_port": 80,
+            "cidr_blocks": ["0.0.0.0/0"],
+        }
+    ],
+)
+
+lb = aws.lb.LoadBalancer(
+    "loadbalancer",
+    internal=False,
+    security_groups=[group.id],
+    subnets=vpc_subnets.ids,
+    load_balancer_type="application",
+)
+
+target_group = aws.lb.TargetGroup(
+    "target-group", port=80, protocol="HTTP", target_type="ip", vpc_id=vpc.id
+)
+
+listener = aws.lb.Listener(
+    "listener",
+    load_balancer_arn=lb.arn,
+    port=80,
+    default_actions=[{"type": "forward", "target_group_arn": target_group.arn}],
 )
 
 USER_DATA = """
 #!/bin/bash
-echo "Hello, World!" > index.html
+echo \"Hello, World -- from {}!\" > index.html
 nohup python -m SimpleHTTPServer 80 &
 """
 
@@ -42,19 +78,26 @@ hostnames = []
 
 for az in aws.get_availability_zones().names:
     server = aws.ec2.Instance(
-            
-            f'web-server-{az}',
-            instance_type="t2.micro",
-            vpc_security_group_ids=[group.id],
-            ami=ami.id,
-            availability_zone=az,
-            user_data=USER_DATA,
-            tags={
-                "Name": "web-server",
-            },
+        f"web-server-{az}",
+        instance_type="t2.micro",
+        vpc_security_group_ids=[group.id],
+        ami=ami.id,
+        user_data=USER_DATA.format(az),
+        availability_zone=az,
+        tags={
+            "Name": "web-server",
+        },
     )
     ips.append(server.public_ip)
     hostnames.append(server.public_dns)
 
+    attachment = aws.lb.TargetGroupAttachment(
+        f"web-server-{az}",
+        target_group_arn=target_group.arn,
+        target_id=server.private_ip,
+        port=80,
+    )
+
 pulumi.export("ips", ips)
 pulumi.export("hostnames", hostnames)
+pulumi.export("url", lb.dns_name)
