13c864acbe5653a1c10fedbd3a7afcd4e5142cad
stoo johnston <stoo@pulumi.com>
2024-04-29 10:32:40 +0100

    5.3 API GATEWAY

--- onboarding/aws_iac-workshop_mixed/lambda-api-gateway/index.ts
+++ onboarding/aws_iac-workshop_mixed/lambda-api-gateway/index.ts
@@ -1,3 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+
+const api = new awsx.classic.apigateway.API("hello-world", {
+    routes: [{
+        path: "/",
+        method: "GET",
+        eventHandler: async (event) => {
+            return {
+                statusCode: 200,
+                body: "Hello, world!",
+            };
+        },
+    }],
+})
+
+export const url = api.url;
