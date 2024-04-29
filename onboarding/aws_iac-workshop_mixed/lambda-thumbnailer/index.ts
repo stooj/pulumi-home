96883c3d0f12d7c513fa130ae0305500f3d8e6e7
stoo johnston <stoo@pulumi.com>
2024-04-29 11:51:36 +0100

    6.3 thumbnailer container

--- onboarding/aws_iac-workshop_mixed/lambda-thumbnailer/index.ts
+++ onboarding/aws_iac-workshop_mixed/lambda-thumbnailer/index.ts
@@ -2,4 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-
+const image = awsx.classic.ecr.buildAndPushImage("thumbnailer", {
+    context: "./app",
+});
