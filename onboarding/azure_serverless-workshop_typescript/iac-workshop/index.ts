12e338f95bbc09d520ff38b1190ee99806cd8102
stoo johnston <stoo@pulumi.com>
2024-05-07 12:18:36 +0100

    azure-serverless-workshop: 04-updating-your-infrastructure

--- onboarding/azure_serverless-workshop_typescript/iac-workshop/index.ts
+++ onboarding/azure_serverless-workshop_typescript/iac-workshop/index.ts
@@ -6,3 +6,21 @@ const resourceGroup = new resources.ResourceGroup("my-group", {
     resourceGroupName: "my-group",
     location: "westus",
 });
+
+const storageAccount = new storage.StorageAccount("mystorage", {
+    resourceGroupName: resourceGroup.name,
+    accountName: "c01ab8082e05",
+    location: resourceGroup.location,
+    sku: {
+        name: "Standard_LRS",
+    },
+    kind: "StorageV2",
+});
+
+const container = new storage.BlobContainer("mycontainer", {
+    resourceGroupName: resourceGroup.name,
+    accountName: storageAccount.name,
+    containerName: "files",
+});
+
+export const accountName = storageAccount.name;
