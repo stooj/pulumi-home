a812b5846d6f2cb8946140de601264ae722fb012
stoo johnston <stoo@pulumi.com>
2024-05-07 11:58:41 +0100

    azure-serverless-workshop: 03-provisioning-infrastructure

--- onboarding/azure_serverless-workshop_typescript/iac-workshop/index.ts
+++ onboarding/azure_serverless-workshop_typescript/iac-workshop/index.ts
@@ -1,3 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-native/resources/latest";
-import * as storage from "@pulumi/azure-native/storage/latest";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+const resourceGroup = new resources.ResourceGroup("my-group", {
+    resourceGroupName: "my-group",
+    location: "westus",
+});
