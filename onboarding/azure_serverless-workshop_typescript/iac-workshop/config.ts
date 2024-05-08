82880923cdb6b82cde36ab164785b73618d2bb95
stoo johnston <stoo@pulumi.com>
2024-05-08 14:23:38 +0100

    azure-serverless-workshop: 02-01-deploying-serverless-applications-with-azure-functions

--- onboarding/azure_serverless-workshop_typescript/iac-workshop/config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@pulumi/pulumi";
-
-const config = new Config();
-export const containerName = config.require("container");
-export const account = config.require("account");
-export const resourceGroupName = config.require("resourceGroup");
--- onboarding/azure_serverless-workshop_typescript/iac-workshop/index.ts
+++ onboarding/azure_serverless-workshop_typescript/iac-workshop/index.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
-import { account, containerName, resourceGroupName } from "./config";
+import * as web from "@pulumi/azure-native/web";
 
 const resourceGroup = new resources.ResourceGroup("my-group", {
-    resourceGroupName: resourceGroupName,
+    resourceGroupName: "my-group",
     location: "westus",
 });
 
 const storageAccount = new storage.StorageAccount("mystorage", {
     resourceGroupName: resourceGroup.name,
-    accountName: account,
+    accountName: "f3af16454f99",
     location: resourceGroup.location,
     sku: {
         name: "Standard_LRS",
@@ -18,10 +18,41 @@ const storageAccount = new storage.StorageAccount("mystorage", {
     kind: "StorageV2",
 });
 
-const container = new storage.BlobContainer("mycontainer", {
+const plan = new web.AppServicePlan("asp", {
     resourceGroupName: resourceGroup.name,
-    accountName: storageAccount.name,
-    containerName: containerName,
+    name: "consumption-plan",
+    location: resourceGroup.location,
+    sku: {
+        name: "Y1",
+        tier: "Dynamic",
+    },
 });
 
-export const accountName = storageAccount.name;
+const storageAccountKeys = pulumi.all(
+    [resourceGroup.name, storageAccount.name]
+).apply(([resourceGroupName, accountName]) => storage.listStorageAccountKeys(
+    { resourceGroupName, accountName })
+);
+
+const primaryStorageKey = storageAccountKeys.keys[0].value;
+const storageConnectionString = pulumi.interpolate`DefaultEndpointsProtocol=https;AccountName=${storageAccount.name};AccountKey=${primaryStorageKey}`;
+
+const app = new web.WebApp("fa", {
+    resourceGroupName: resourceGroup.name,
+    name: "7675de5e140e",
+    location: resourceGroup.location,
+    serverFarmId: plan.id,
+    kind: "functionapp",
+    siteConfig: {
+        appSettings: [
+            { name: "AzureWebJobsStorage", value: storageConnectionString },
+            { name: "FUNCTIONS_EXTENSION_VERSION", value: "~3" },
+            { name: "FUNCTIONS_WORKER_RUNTIME", value: "node" },
+            { name: "WEBSITE_NODE_DEFAULT_VERSION", value: "10.14.1" },
+            { name: "WEBSITE_RUN_FROM_PACKAGE", value: "https://mikhailworkshop.blob.core.windows.net/zips/app.zip" },
+
+        ]
+    },
+});
+
+export const endpoint = pulumi.interpolate`https://${app.defaultHostName}/api/hello`;
